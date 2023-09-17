718280540ba31e1ec93c48be1ac6cf2571c56738
Chris Horsley <chris.horsley@cosive.com>
2023-09-18 08:36:30 +1000

    Auto-detect input type rather than user having to fiddle with args; can override with --force-content-type

--- lib/runner.py
+++ lib/runner.py
@@ -4,6 +4,9 @@ Main runner for the summariser separate from the CLI.
 import pprint
 import argparse
 from typing import List
+from enum import Enum
+from urllib.parse import urlparse
+import mimetypes
 
 from .logger import logger
 
@@ -11,24 +14,76 @@ from .tokenizer import Tokenizer
 from .llm_summariser import Summarizer
 from .audio_handler import AudioHandler
 
+
+class ContentType(Enum):
+    """
+    Different types of content we can handle
+    """
+    YOUTUBE_URL = "youtube-url"
+    URL = "url"
+    AUDIO_FILE = "audio-file"
+    TEXT_FILE = "text-file"
+
+
+def detect_content_type(input_path: str) -> ContentType:
+    """
+    Detect the content type of the input.
+    """
+    # see if it's a URL or YouTube URL
+    if input_path.startswith("http"):
+        parsed_url = urlparse(input_path)
+
+        if parsed_url.netloc.endswith("youtube.com"):
+            return ContentType.YOUTUBE_URL
+        elif parsed_url.scheme in ["http", "https"]:
+            return ContentType.URL
+
+    # fallback to file checking
+    file_type, _ = mimetypes.guess_type(input_path)
+
+    if file_type is None:
+        raise ValueError(f"Could not detect content type of input. Check it exists: {input_path}")
+    elif file_type.startswith("audio/"):
+        return ContentType.AUDIO_FILE
+    elif file_type.startswith("text/"):
+        return ContentType.TEXT_FILE
+    elif file_type.startswith("video/"):
+        return ContentType.URL
+    else:
+        raise ValueError(f"Could not detect content type of input: {input_path}")
+
+
 def summarise(args: argparse.Namespace):
     """
     Handle args and run summariser.
     """
     text_filename:str = ""
     summaries:List[str] = []
+    input_location = getattr(args, "input-location")
+    
+    print(args)
+    if args.force_content_type:
+        try:
+            content_type = ContentType(args.force_content_type)
+        except KeyError:
+            raise ValueError(f"Unsupported content type: {args.force_content_type}")
+    else:
+        content_type = detect_content_type(input_location)
 
-    if args.input_youtube_url:
-        audio_filename = AudioHandler.download_from_yt(args.input_youtube_url)
+    if content_type == ContentType.YOUTUBE_URL:
+        audio_filename = AudioHandler.download_from_yt(input_location)
         audio_chunk_filenames = AudioHandler.split_into_chunks(audio_filename)
         logger.debug(f"Created audio file chunks from YouTube URL: {audio_chunk_filenames}")
         text_filename = AudioHandler.create_transcript(audio_chunk_filenames)
-    elif args.input_audio_file:
-        audio_chunk_filenames = AudioHandler.split_into_chunks(args.input_audio_file)
+    elif content_type == ContentType.AUDIO_FILE:
+        audio_chunk_filenames = AudioHandler.split_into_chunks(input_location)
         logger.debug(f"Created audio file from file: {audio_chunk_filenames}")
         text_filename = AudioHandler.create_transcript(audio_chunk_filenames)
+    elif content_type == ContentType.TEXT_FILE:
+        text_filename = input_location
     else:
-        text_filename = args.input_transcript
+        raise ValueError(f"Unsupported content type: {content_type}")
+
     logger.debug(f"Processing {text_filename}")
 
     with open(text_filename, 'r', encoding="utf-8") as f_text_filename:
--- scripts/run.py
+++ scripts/run.py
@@ -17,16 +17,15 @@ def main():
     """
     Main entry point for the summariser.
     """
-    parser = argparse.ArgumentParser(description="Process a file path or YouTube URL.")
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("--input-transcript", type=str, help="Transcript file to process.")
-    group.add_argument("--input-audio-file", type=str, help="Audio file to process.")
-    group.add_argument("--input-youtube-url", type=str, help="YouTube URL to process.")
+    parser = argparse.ArgumentParser(description="Process a file path or URL.")
+    parser.add_argument("input-location", type=str, help="File path or URL to process.")
     parser.add_argument("--debug", action="store_true", help="Enable debug logging.")
     parser.add_argument("--prompt", type=str, help="Prompt to use for GPT model")
     parser.add_argument("--model", type=str, default="gpt-3.5-turbo",
                          choices=["gpt-3.5-turbo", "gpt-4"],
                          help="OpenAI model to use for summarisation.")
+    parser.add_argument("--force-content-type", type=str, choices=["youtube-url", "url", "audio-file", "text-file"],
+                        help="Force the content type to be one of these options.")
 
     args = parser.parse_args()
 
